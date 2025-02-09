1fb955d7872d0d5528950391f748e0794fc86b66
Etobimz <etooluwa200@gmail.com>
2025-02-08 20:41:59 -0700

    char

---
+++ f.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() { 
+    char name[20];
+    char fullname[90];
+
+printf("Input name: ");
+scanf("%s", name);
+
+while (getchar() !=  '\n');
+
+printf("Input full name: ");
+fgets(fullname, sizeof(fullname), stdin);
+
+printf("Name is: %s\n", name);
+printf("Fullname is: %s\n", fullname);
+
+return 0;
+
+
+
+}
